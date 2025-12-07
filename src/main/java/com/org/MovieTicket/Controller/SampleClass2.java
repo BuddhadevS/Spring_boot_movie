bf2a410d1283cebd31bc6cb00796836cadc42730
buddhadevS <buddhadev0509@gmail.com>
2025-12-07 11:18:17 +0530

    add sampleclass 2

---
+++ src/main/java/com/org/MovieTicket/Controller/SampleClass2.java
@@ -0,0 +1,8 @@
+package com.org.MovieTicket.Controller;
+
+public class SampleClass2 {
+    public static void main(String[] args) {
+        String s  = "Hello, World!";
+        System.out.println(s);
+    }
+}
